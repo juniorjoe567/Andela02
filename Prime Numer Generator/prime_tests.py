fbafae2aeb205e81330913001307cee6aaffa9b1
Kenneth Lebu <kenlebu@gmail.com>
2017-07-03 12:24:26 +0300

    Added test for prime numbers

--- Prime Numer Generator/prime_tests.py
+++ Prime Numer Generator/prime_tests.py
@@ -2,3 +2,8 @@ import unittest
 from primes import PrimeGenerator
 
 class PrimeGeneratorTests(unittest.TestCase):
+
+    """ Testing whether all returned numbers are prime """
+    def test_primes(self):
+        self.assertEqual(PrimeGenerator(10).prime_generator(), [2, 3, 5, 7])
+        self.assertEqual(PrimeGenerator(20).prime_generator(), [2, 3, 5, 7, 11, 13, 17, 19])
