88e1fd58e539aa42209a9010f51591b600ff3708
Kenneth Lebu <kenlebu@gmail.com>
2017-07-03 14:25:59 +0300

    Added function to generate prime numbers

--- Prime Numer Generator/primes.py
+++ Prime Numer Generator/primes.py
@@ -1,3 +1,21 @@
 """ Class that generates prime numbers from 0 to n """
 
 class PrimeGenerator:
+    def __init__(self, number):
+        self.number = number
+
+    def prime_generator():
+        primes_list = []
+        
+        # Add the number to the list if it is a prime number
+        for integer in range(2, number + 1):
+            if is_prime(integer):
+                primes_list.append(integer)
+
+        return primes_list
+
+    """ Takes in an integer and returns true if it is a prime number """
+    def is_prime(integer):
+        for each in range(2, integer):
+            if integer % each == 0:
+                return false
