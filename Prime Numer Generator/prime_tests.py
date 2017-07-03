1ccc18f0b6e59adfdb008134f06e72ed1b5f4726
Kenneth Lebu <kenlebu@gmail.com>
2017-07-03 12:26:43 +0300

    Added test for 0

--- Prime Numer Generator/prime_tests.py
+++ Prime Numer Generator/prime_tests.py
@@ -7,3 +7,7 @@ class PrimeGeneratorTests(unittest.TestCase):
     def test_primes(self):
         self.assertEqual(PrimeGenerator(10).prime_generator(), [2, 3, 5, 7])
         self.assertEqual(PrimeGenerator(20).prime_generator(), [2, 3, 5, 7, 11, 13, 17, 19])
+
+    """ Testing for zero """
+    def test_zero(self):
+        self.assertEqual(PrimeGenerator(0).prime_generator(), "Zero is not prime")
